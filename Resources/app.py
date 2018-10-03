fecf0b8785afc520a634a7ea3aa41768c7301237
disco_nap <jrnapolit@gmail.com>
2018-10-03 13:44:28 -0700

    added flask API for climate queries

---
+++ Resources/app.py
@@ -0,0 +1,96 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
+
+
+# Database Setup
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Measurement = Base.classes.measurement
+
+Station = Base.classes.station
+
+# Create our session (link) from Python to the DB
+session = Session(engine)
+
+# Flask Setup
+app = Flask(__name__)
+
+
+
+# Flask Routes
+
+@app.route("/")
+def welcome():
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end>"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    precip_year = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date.between('2016-01-01', '2017-01-01')).all()
+
+    precip = dict(precip_year)
+
+    return jsonify(precip)
+
+
+@app.route("/api/v1.0/stations")
+def stations():
+    #Return a JSON list of stations from the dataset.
+    stations = session.query(func.distinct(Measurement.station)).all()
+
+    return jsonify(stations)
+
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    #Return a JSON list of Temperature Observations (tobs) for the previous year.
+    temps = session.query(Measurement.date, Measurement.tobs).\
+        filter(Measurement.date.between('2016-01-01' , '2017-01-01')).all()
+
+    temp = dict(temps)
+
+    return jsonify(temp)
+
+@app.route("/api/v1.0/<start>")
+def start():
+    #Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
+#When given the start only, calculate TMIN, TAVG, and TMAX for all dates greater than and equal to the start date.
+#When given the start and the end date, calculate the TMIN, TAVG, and TMAX for dates between the start and end date inclusive.
+    temps = session.query(Measurement.date, Measurement.tobs).\
+        filter(Measurement.date.between('2016-01-01' , '2017-01-01')).all()
+
+    temp = dict(temps)
+
+    return jsonify(temp)
+
+@app.route("/api/v1.0/<start>/<end>")
+def startend():
+    #Return a JSON list of Temperature Observations (tobs) for the previous year.
+    temps = session.query(Measurement.date, Measurement.tobs).\
+        filter(Measurement.date.between('2016-01-01' , '2017-01-01')).all()
+
+    temp = dict(temps)
+
+    return jsonify(temp)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
